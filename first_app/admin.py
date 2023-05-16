4c9c71dfea272d4775f31a998d3bc5e0dfe83914
random name <testmai@gmail.com>
2023-05-16 21:34:29 +0200

    lets start again

--- first_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from first_app.models import AccessRecord, Webpage, Topic, UserAll, UserProfileInfo
-# Register your models here.
-
-admin.site.register(AccessRecord)
-admin.site.register(Topic)
-admin.site.register(Webpage)
-admin.site.register(UserAll)
-admin.site.register(UserProfileInfo)
