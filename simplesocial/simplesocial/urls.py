e890ae3018c95f4020ab85149ff42b461627dfb8
random name <testmai@gmail.com>
2023-05-28 12:11:36 +0200

    some fixes, added homepage view, added html-s

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from simplesocial.views import HomePage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePage.as_view(), name='home')
+    path('', HomePage.as_view(), name='home'),
+    path('accounts/', include('accounts.urls', namespace='accounts')),
+    path('accounts', include('django.contrib.auth.urls')),
 ]
