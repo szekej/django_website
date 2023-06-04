be3f8cf2b35c3ec0df9df6b4adfde0e6c0286f68
random name <testmai@gmail.com>
2023-06-04 15:56:19 +0200

    fixes after debugging

--- simplesocial/posts/urls.py
+++ simplesocial/posts/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = 'posts'
 
-url_patterns = [
+urlpatterns = [
     path(r'', views.PostList.as_view(), name='all'),
     path(r'new/', views.CreatePost.as_view(), name='create'),
-    path(r'by/(?P<username>[-\w]+)/', views.UserPosts.as_view(), name='for_user'),
-    path(r'by/(?P<username>[-\w]+)/(?P<pk>\d)/', views.PostDetail.as_view(), name='single'),
-    path(r'delete/(?P<pk>\d)/', views.DeletePost.as_view(), name='delete'),
+    path(r'by/(username[-\w]+)/', views.UserPosts.as_view(), name='for_user'),
+    path(r'by/(username[-\w]+)/(pk\d)/', views.PostDetail.as_view(), name='single'),
+    path(r'delete/(pk\d)/', views.DeletePost.as_view(), name='delete'),
 ]
--- simplesocial/simplesocial/settings.py
+++ simplesocial/simplesocial/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrap3',
     'accounts',
+    'groups',
+    'posts',
 ]
 
 MIDDLEWARE = [
